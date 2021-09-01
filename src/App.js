75a275761ff99c402e59f2f986db050f2c5e826a
davidcher86 <davidcher86@gmail.com>
2021-09-01 17:32:02 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,152 @@
+import "./styles.css";
+
+import { useEffect, useState } from "react";
+import {
+  SimpleTextBlock,
+  ImageBlock,
+  CollapsabaleBlock,
+  HTMLBlock,
+  QuoteBlock,
+  TableBlock
+} from "./shared.js";
+
+export default function App() {
+  const [documentLayout, setDocumentLayout] = useState(null);
+  const [rollbackData, setRollbackData] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+
+  useEffect(() => {
+    fetchDocument(); // loading document's data on component first mount
+  }, []);
+
+  // function for handle collapsable blocks change of collapse state
+  const toggleColapseble = (e) => {
+    const updated = Object.assign({}, documentLayout);
+    updated[e.target.id].isOpen = !documentLayout[e.target.id].isOpen;
+    setDocumentLayout(updated);
+  };
+
+  // function for handle update text/collapsable block's title
+  const handleUpdateTitle = (e, key) => {
+    const updated = Object.assign({}, documentLayout);
+    updated[key].data.title = e.target.value;
+    setDocumentLayout(updated);
+  };
+
+  // adjust json of loaded document for parsing purposes by the app
+  const loadDocument = (data) => {
+    const orderedBlocks = Object.keys(data.documentLayout)
+      .sort((a, b) =>
+        // sorting blocks key by block's sort parameters, to have block displayed by their order
+        // sorting document hash map by re-ordering map keys
+        data.documentLayout[a].order > data.documentLayout[b].order ? 1 : -1
+      )
+      .reduce((obj, key) => {
+        let val = data.documentLayout[key];
+
+        if (val.blockType === "collapsabale") {
+          // adding state param for collapsable block items
+          val = Object.assign({}, val, { isOpen: false });
+        }
+
+        // adding Id param for each  block item that represents thair key in document's map object
+        obj[key] = Object.assign({}, val, { id: key });
+        return obj;
+      }, {});
+    console.log(orderedBlocks);
+    setDocumentLayout(orderedBlocks);
+  };
+
+  // function for handle turn on of edit mode
+  const handleToggleEditMode = () => {
+    // creating initial document status before
+    // any change to have a rollback version in case of discard action /
+    setRollbackData(Object.assign({}, documentLayout));
+    setEditMode(true);
+  };
+
+  // function for handle saving the changed document
+  const handleSave = () => {
+    setEditMode(false);
+  };
+
+  // function for handle disacrd of changes
+  const handleDisacrd = () => {
+    setEditMode(false);
+    // setting document to it's pre change state
+    setDocumentLayout(rollbackData);
+    setRollbackData(null);
+  };
+
+  // function for loading the document from public folder to mimic proces of
+  // loading document from server
+  const fetchDocument = () => {
+    fetch(`document.json`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    })
+      .then((r) => r.json())
+      .then((res) => {
+        loadDocument(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const listBlock = (data) => {
+    if (data) {
+      return Object.values(data).map((item) => {
+        switch (item.blockType) {
+          case "simpleText":
+            return SimpleTextBlock(item, editMode, handleUpdateTitle);
+          case "image":
+            return ImageBlock(item);
+          case "quote":
+            return QuoteBlock(item);
+          case "table":
+            return TableBlock(item);
+          case "html":
+            return HTMLBlock(item);
+          case "collapsabale":
+            return CollapsabaleBlock(
+              item,
+              editMode,
+              toggleColapseble,
+              handleUpdateTitle
+            );
+          default:
+            return null;
+        }
+      });
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <div className="App">
+      <div id="app-header" className="page-header">
+        <h1>Document Render</h1>
+        {!editMode && (
+          <button className="edit-btn" onClick={handleToggleEditMode}>
+            edit
+          </button>
+        )}
+        {editMode && (
+          <button className="edit-btn" onClick={handleSave}>
+            save
+          </button>
+        )}
+        {editMode && (
+          <button className="edit-btn" onClick={handleDisacrd}>
+            discard
+          </button>
+        )}
+      </div>
+      {documentLayout && listBlock(documentLayout)}
+    </div>
+  );
+}
---
+++ src/shared.js
@@ -0,0 +1,159 @@
+import React from "react";
+
+const BLOCK_SIZES = ["100%", "66%", "50%", "33%"]; // block size array for documnt layout
+
+const title = (text) => <div className="title">{text}</div>;
+
+/** All types of block that document can have as shared resources */
+
+// Simple text block
+export function SimpleTextBlock(block, editMode, updateTitle) {
+  return (
+    <div
+      className="block-wrapper"
+      style={{ width: BLOCK_SIZES[block.size - 1] }}
+    >
+      <div
+        id={block.data.id}
+        key={block.data.id}
+        className="block columns container margin"
+      >
+        <div className="block-type">{block.blockType}</div>
+        {!editMode && title(block.data.title)}
+        {editMode && (
+          <input
+            onChange={(e) => updateTitle(e, block.id)}
+            defaultValue={block.data.title}
+          />
+        )}
+        <div className="content">{block.data.content}</div>
+      </div>
+    </div>
+  );
+}
+
+// Image block
+export function ImageBlock(block) {
+  return (
+    <div
+      className="block-wrapper"
+      style={{ width: BLOCK_SIZES[block.size - 1] }}
+    >
+      <div id={block.id} key={block.id} className="block rows container margin">
+        <div className="columns">
+          <div className="block-type">{block.blockType}</div>
+          <img srcSet={`${block.data.url} 0.5x`} alt={block.data.altText} />
+          <div>{block.data.caption}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// Quote block
+export function QuoteBlock(block) {
+  return (
+    <div
+      className="block-wrapper"
+      style={{ width: BLOCK_SIZES[block.size - 1] }}
+    >
+      <div id={block.id} key={block.id} className="block margin columns">
+        <div className="block-type">{block.blockType}</div>
+        <span style={{ fontWeight: "bold" }}>{block.data}</span>
+      </div>
+    </div>
+  );
+}
+
+// Table block
+export function TableBlock(block) {
+  // Table rows render
+  const renderTableRows = (tableRows) => {
+    let index = 1;
+    if (tableRows !== undefined) {
+      return tableRows.map((row) => {
+        index = index + 0.1;
+
+        return (
+          <tr key={`row=${index}`}>
+            <td>{row.col1}</td>
+            <td>{row.col2}</td>
+            <td>{row.col3}</td>
+          </tr>
+        );
+      });
+    } else {
+      return null;
+    }
+  };
+
+  // Table header render
+  const renderTableHeader = () => {
+    return (
+      <tr>
+        <th>{`col1`}</th>
+        <th>{`col2`}</th>
+        <th>{`col3`}</th>
+      </tr>
+    );
+  };
+
+  return (
+    <div
+      className="block-wrapper"
+      style={{ width: BLOCK_SIZES[block.size - 1] }}
+    >
+      <div className="block margin" id={block.id} key={block.id}>
+        <span className="block-type">{block.blockType}</span>
+        <table className="table">
+          <thead>{renderTableHeader()}</thead>
+          <tbody>{renderTableRows(block.data)}</tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+// Html block
+export function HTMLBlock(block) {
+  // console.log("HTMLBlock");
+  return (
+    <div
+      className="block-wrapper"
+      style={{ width: BLOCK_SIZES[block.size - 1] }}
+    >
+      <div id={block.id} key={block.id} className="block columns margin ">
+        <div className="block-type">{block.blockType}</div>
+        <div dangerouslySetInnerHTML={{ __html: block.data }} />
+      </div>
+    </div>
+  );
+}
+
+// Collapsabale text block
+export function CollapsabaleBlock(block, editMode, toggleAction, updateTitle) {
+  return (
+    <div
+      className="block-wrapper"
+      style={{ width: BLOCK_SIZES[block.size - 1] }}
+    >
+      <div
+        key={`${block.id}`}
+        id={block.id}
+        className="block columns container margin"
+      >
+        <div className="block-type">{block.blockType}</div>
+        <button id={block.id} className="add-btn" onClick={toggleAction}>
+          {block.isOpen ? "- hide" : "+ show"}
+        </button>
+        <div className={`collapse-content ${block.isOpen ? "show" : "hide"}`}>
+          {!editMode && title(block.data.title)}
+          {editMode && (
+            <input onChange={updateTitle} defaultValue={block.data.title} />
+          )}
+          <div className="content">{block.data.content}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
